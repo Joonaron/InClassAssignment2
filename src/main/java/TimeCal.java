6a2f26f34df60bf02fdfac6db4bf43a0a379ae36
Joonas <Joonas.Ronimus@metropolia.fi>
2025-02-27 13:32:29 +0200

    initial commit

---
+++ src/main/java/TimeCal.java
@@ -0,0 +1,8 @@
+public class TimeCal {
+    public static void main(String[] args){
+        System.out.println("Time needed: " + timeCalculator(100,100));
+    }
+    public static double timeCalculator(double x, double y){
+        return x/y;
+    }
+}
